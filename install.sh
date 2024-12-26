6c605d42c81884ec817b8b0a4866d7dbfcf401e7
Aron Høyer <aron@hoyer.com>
2024-12-26 04:13:47 +0100

    ayrtienr

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+sudo apt update
+
 echo "Installing nala"
 sudo apt update && sudo apt install nala && sudo nala upgrade
 
+sudo nala update
+
 echo "Installing packages from $PWD/packages.txt"
-sudo nala install "$(cat ./packages.txt | head -c -1 | tr '\n' ' ')"
+sudo nala install -y "$(cat ./packages.txt | head -c -1 | tr '\n' ' ')"
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
@@ -22,18 +26,22 @@ function get_latest_git_tag() {
 	git describe --tags "$(git rev-list --tags --max-count=1)"
 }
 
+# install neovim <3
 git clone https://github.com/neovim/neovim.git $PKG_PREFIX/neovim
 
 pushd $PKG_PREFIX/neovim
 git switch --detach $(get_latest_git_tag)
 
+sudo nala install -y ninja-build gettext cmake unzip curl build-essential
+
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 cpack -G DEB
-sudo nala install ./build/nvim-linux64.deb
+sudo nala install -y ./build/nvim-linux64.deb
 popd
 
 git clone https://github.com/aronhoyer/init.lua.git $HOME/.config/nvim
 
+# install alacritty <3
 git clone https://github.com/alacritty/alacritty.git $PKG_PREFIX/alacritty
 
 pushd $PKG_PREFIX/alacritty
@@ -49,6 +57,8 @@ sudo update-desktop-database
 
 sudo mkdir -v -p /usr/local/share/man/man1
 sudo mkdir -v -p /usr/local/share/man/man5
+
+sudo nala install -y scdoc
 
 scdoc < extra/man/alacritty.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
 scdoc < extra/man/alacritty-msg.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
@@ -66,3 +76,22 @@ ln -s $SCRIPT_DIR/.config/* $HOME/.config
 cp ./pictures/* $HOME/Pictures
 
 # TODO: install Nordic-darker theme
+
+# uninstall conflicting packages for docker
+sudo nala remove -y docker.io docker-doc docker-compose podman-docker containerd runc
+
+# Add Docker's official GPG key:
+sudo nala install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo nala update
+
+# install required packages
+sudo nala install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
